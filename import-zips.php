bd5da4cd2839d0d70ccd1e1213daa36dece5bd06
kiwi-gherardo-bertini <gherardo.bertini@kiwitron.it>
2021-12-02 16:15:32 +0100

    add import zips

---
+++ import-zips.php
@@ -0,0 +1,17 @@
+<?php
+
+$filename = 'https://media.mongodb.org/zips.json';
+$lines = file($filename, FILE_IGNORE_NEW_LINES);
+
+$bulk = new MongoDB\Driver\BulkWrite;
+
+foreach ($lines as $line) {
+    $bson = MongoDB\BSON\fromJSON($line);
+    $document = MongoDB\BSON\toPHP($bson);
+    $bulk->insert($document);
+}
+
+$manager = new MongoDB\Driver\Manager($_ENV['ME_CONFIG_MONGODB_URL']);
+
+$result = $manager->executeBulkWrite('test.zips', $bulk);
+printf("Inserted %d documents\n", $result->getInsertedCount());
